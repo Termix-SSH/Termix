effd679a003b25186c2fbfd3178ee860a23f77e3
LukeGus <bugattiguy527@gmail.com>
2024-11-28 04:51:57 +0000

    Commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     terminal.current.open(terminalRef.current);
 
     // Connect to the WebSocket server
-    socket.current = new WebSocket('2.tcp.ngrok.io:10908');
+    socket.current = new WebSocket('localhost:3001');
 
     // WebSocket Event Handlers
     socket.current.onopen = () => {
