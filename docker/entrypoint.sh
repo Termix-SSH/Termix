5d007e05fd0137f20effbd426e30f591c6d786dc
Karmaa <bugattiguy527@gmail.com>
2025-03-11 23:11:25 -0500

    Changes to Dockerfile to fix error in installing MongoDB

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Start MongoDB (it should already be running from the mongo:5 container)
-echo "Starting MongoDB..."
-# MongoDB will run in a separate container, no need to start it here.
-
 # Start NGINX
 echo "Starting NGINX..."
 nginx -g "daemon off;" &
