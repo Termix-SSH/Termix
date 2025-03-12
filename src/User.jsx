4027c6a041cd97eaa7475b2dbcd947b2fdab0af6
Karmaa <bugattiguy527@gmail.com>
2025-03-11 23:48:50 -0500

    Changes to Dockerfile to fix error in connecting to sockets

--- src/User.jsx
+++ src/User.jsx
@@ -10,7 +10,7 @@ if (socket === null) {
             ? "http://localhost:8082"
             : "/",
         {
-            path: "/database-socket.io",
+            path: "/database-socket.io/",
             transports: ["websocket", "polling"],
         }
     );
