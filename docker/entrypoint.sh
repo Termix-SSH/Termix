aa95fee450d2cd9b94f60b2e347774730a1cb433
Karmaa <bugattiguy527@gmail.com>
2025-03-16 13:07:18 -0500

    Attempt #1 to auto compile MongoDB into the build to exclude it from the compose.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
-# Start NGINX in background
-nginx -g "daemon off;" &
+# Start MongoDB with custom data directory
+mongod --fork --dbpath $MONGODB_DATA_DIR --logpath $MONGODB_LOG_DIR/mongodb.log
 
-# Start Node.js backend
+# Start nginx
+nginx
+
+# Start the SSH service
 node src/backend/ssh.cjs &
+
+# Start the database service
 node src/backend/database.cjs &
 
-# Keep container running
-wait
+# Keep the container running
+tail -f /dev/null
